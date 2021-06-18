637b3f374144b77322dfb1b39b21536b72c59461
luis botelho <luisfellypebotelho@gmail.com>
2021-06-17 21:08:56 -0300

    primeira aula conjunto

---
+++ Aulas/Aula09/Exercicios-em conjunto.py.py
@@ -0,0 +1,61 @@
+###Aula 09 - CodeLab - Estruturas de Repetição e Listas
+#01 - Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já esteja lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+lista = []
+
+# while True:
+    
+#     entrada = n.append(int(input('Digite um número: ')))
+#     if entrada in n:
+#         del entrada
+
+#     condicao = str(input('Deseja continuar? SIM ou NÃO? ').upper().replace('Ã', 'A'))
+    
+
+#     if condicao == 'NAO':
+#         break
+# n.sort()
+# print(n)
+
+
+
+pergunta = str(input('Deseja continuar ? S/N ').upper())
+while pergunta[0] == 'S':
+    entrada = int(input('Digite o valor: '))
+    pergunta = str(input('Deseja continuar ? S/N ').upper())
+    if entrada in lista:
+        pass
+    else: 
+        lista.append(entrada)
+lista.sort()
+print(lista)
+
+
+
+
+#02 - Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+lista = []
+par = []
+impar = []
+
+
+pergunta = str(input('Deseja adicionar um número ? S/N ').upper())
+while pergunta[0] == 'S':
+    entrada = int(input('Digite o valor: '))
+    pergunta = str(input('Deseja continuar ? S/N ').upper())
+    if entrada in lista:
+        pass
+    else: 
+        lista.append(entrada)
+    if entrada % 2 == 0:
+        par.append(entrada)
+    else:
+        impar.append(entrada)
+lista.sort()
+par.sort()
+impar.sort()
+print(lista)
+print(par)
+print(impar)
+
+
+#Desafio: Faça um programa que ajude um jogador da MEGA SENA a criarpalpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
