a060c38bb1fcfe72f350c60392d2e6937e608e35
luis botelho <luisfellypebotelho@gmail.com>
2021-06-17 23:07:23 -0300

    estudos dia17

--- Aulas/Aula09/Exercicios-em conjunto.py.py
+++ Aulas/Aula09/Exercicios-em conjunto.py.py
@@ -1,6 +1,6 @@
 ###Aula 09 - CodeLab - Estruturas de Repetição e Listas
 #01 - Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já esteja lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
-lista = []
+
 
 # while True:
     
@@ -15,47 +15,62 @@ lista = []
 #         break
 # n.sort()
 # print(n)
+# lista = []
+
+
+# pergunta = str(input('Deseja continuar ? S/N ').upper())
+# while pergunta[0] == 'S':
+#     entrada = int(input('Digite o valor: '))
+#     pergunta = str(input('Deseja continuar ? S/N ').upper())
+#     if entrada in lista:
+#         pass
+#     else: 
+#         lista.append(entrada)
+# lista.sort()
+# print(lista)
 
 
 
-pergunta = str(input('Deseja continuar ? S/N ').upper())
-while pergunta[0] == 'S':
-    entrada = int(input('Digite o valor: '))
-    pergunta = str(input('Deseja continuar ? S/N ').upper())
-    if entrada in lista:
-        pass
-    else: 
-        lista.append(entrada)
-lista.sort()
-print(lista)
+
+# #02 - Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+# lista = []
+# par = []
+# impar = []
 
 
+# pergunta = str(input('Deseja adicionar um número ? S/N ').upper())
+# while pergunta[0] == 'S':
+#     entrada = int(input('Digite o valor: '))
+#     pergunta = str(input('Deseja continuar ? S/N ').upper())
+#     if entrada in lista:
+#         pass
+#     else: 
+#         lista.append(entrada)
+#     if entrada % 2 == 0:
+#         par.append(entrada)
+#     else:
+#         impar.append(entrada)
+# lista.sort()
+# par.sort()
+# impar.sort()
+# print(lista)
+# print(par)
+# print(impar)
 
 
-#02 - Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
-lista = []
-par = []
-impar = []
+#Desafio: Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 
 
-pergunta = str(input('Deseja adicionar um número ? S/N ').upper())
-while pergunta[0] == 'S':
-    entrada = int(input('Digite o valor: '))
-    pergunta = str(input('Deseja continuar ? S/N ').upper())
-    if entrada in lista:
-        pass
-    else: 
-        lista.append(entrada)
-    if entrada % 2 == 0:
-        par.append(entrada)
-    else:
-        impar.append(entrada)
-lista.sort()
-par.sort()
-impar.sort()
-print(lista)
-print(par)
-print(impar)
+import random
+from typing import final
 
-
-#Desafio: Faça um programa que ajude um jogador da MEGA SENA a criarpalpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+jogo = []
+final = []
+var = int(input('Digite a quantidade de jogos'))
+for y in range(var):
+    for i in range(6):
+        if i in jogo:
+            pass
+        else:
+            jogo.append(random.randint (1,60))
+print(jogo)
