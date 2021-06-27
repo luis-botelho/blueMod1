276865492f25bdbeb7f86a8af0ee0a5186133810
luis botelho <luisfellypebotelho@gmail.com>
2021-06-27 03:49:49 -0300

    mais alguns toques no projeto

--- projetos/projeto3/Projeto_3_Luis_Fellype_Botelho.py
+++ projetos/projeto3/Projeto_3_Luis_Fellype_Botelho.py
@@ -1,20 +1,7 @@
-# Projeto 03 – Simulador de Votação
-# Crie um programa que simule um sistema de votação, ele deve receber votos até que o usuário diga que não tem mais ninguém para votar, esse programa
-# precisa ter duas funções:
-# A 1° Função precisa ser chamada autoriza_voto() ela vai receber como parâmetro o ano de nascimento de uma pessoa que será digitado pelo usuário, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições. 
-# A 2° Função será a votacao(), ela vai receber dois parâmetros, autorização (que virá da função autoriza_voto()) e o voto que é o número que a pessoa votou. Se ela não puder votar, a 2° função terá que retornar “Você não pode votar”, caso o contrário a 2° função deve validar o número que a pessoa escolheu, ela pode escolher de 1 a 5 (crie 3 candidatos para a votação):
-# 1, 2 ou 3 - Votos para os respectivos candidatos
-# 4- Voto Nulo
-# 5 - Voto em Branco
-# Sua função votacao() tem que calcular e mostrar:
-# O total de votos para cada candidato;
-# O total de votos nulos;
-# O total de votos em branco;
-# Qual candidato venceu a votação.
-#porfessor, desculpe mas fiz 3 funções, porque não vi sentindo na pessoa votar depois validar ou não o voto baseado na idade, me atrapalhou na lógica, espero não perder ponto por isso lol, alem disso o código ficou mais bonitinho e estruturado ^-^
-from functions import autorizaVoto, votacao, apuracao
+#Eu fiz uma bagunça aqui, começei a fazer no pensamento "Não faz sentido eu pegar o voto de quem não tem idade para votar", resumo : acabei com 5 funções, como estou com pouco tempo para fazer exercícios foi bom quebrar a cabeça aqui para treinar, vou estudar mais!
+from functions import autorizaVoto, votacao, apuracao,result, winner
+#Variável para continuar a receber votos até que digite não
 condition = ""
-decision = {}
 #dict com os nomes dos candidatos
 candidates = {'Bolsonaro': 0, 'Lula': 0, 'Batman': 0}
 #dict com os votos em brancos (Depois de pequisar descobri que na verdade os votos nulos e em branco só servem para questões estatisticas)
@@ -24,23 +11,14 @@ print('=========================================================================
 print('Bem vindo(a) as eleições 2022')
 #Começa o programa perguntando o nome para uma saldação amistosa.
 while condition != "NAO":
-    name = str(input('Qual é seu nome ?\n').title().strip())
+    name = str(input('Qual é seu nome ?\n').title().strip()) 
     year = int(input(f'Olá {name}, em qual ano Você nasceu ?\n'))
-    age = 2021 - year
+    age = 2021 - year 
     vote = votacao(autorizaVoto(age))
-
-    if apuracao(vote) == 'Bolsonaro':
-        candidates['Bolsonaro'] += 1
-    elif apuracao(vote)== 'Lula':
-        candidates['Lula'] += 1
-    elif apuracao(vote)== 'Batman':
-        candidates['Batman'] += 1
-    elif apuracao(vote)=='Nulo':
-        null['Nulo'] += 1
-    elif apuracao(vote)== 'Branco':
-        null['Branco'] += 1
-    
+    result(apuracao(vote), candidates, null)
     condition = str(input('Tem mais alguém para votar ?\n').upper().replace(',','.'))
-print(candidates, null)
+winner(candidates,null)
+
+
 
 
--- projetos/projeto3/functions.py
+++ projetos/projeto3/functions.py
@@ -1,5 +1,7 @@
-
+from operator import itemgetter
+from time import sleep
 def autorizaVoto(year):
+    """Valida a idade do usuário"""
     if year < 16:
       return 'Negado'
     elif year >= 18 and year < 71:
@@ -7,6 +9,7 @@ def autorizaVoto(year):
     elif year < 16 or year > 70:
       return 'Opcional'
 def votacao(validate):
+    """Usa a validação do usuário e resgitra a opção de voto"""
     vote = 0
     if validate == 'Negado':
         print(f'Infelizmente Você ainda é muito novo para poder votar!')
@@ -27,6 +30,7 @@ def votacao(validate):
             print('Você digitou algo errado, voto cancelado!')          
     return vote
 def apuracao(choice):
+    """Transforma o valor int digitado em um string para poder adicionar no dicionário"""
     candidate = ""
     if choice == 1:
         candidate = 'Bolsonaro'
@@ -39,6 +43,36 @@ def apuracao(choice):
     elif choice == 5:
         candidate = 'Branco'
     return candidate
-
+def result(vote,available,unavailable):
+    """Adiciona conforme o voto valor ao dicionário"""
+    if vote == 'Bolsonaro':
+        available['Bolsonaro'] += 1
+    elif vote == 'Lula':
+        available['Lula'] += 1
+    elif vote == 'Batman':
+        available['Batman'] += 1
+    elif vote =='Nulo':
+        unavailable['Nulo'] += 1
+    elif vote == 'Branco':
+        unavailable['Branco'] += 1
+def winner(available,unavailable):
+    """fase final, transformando o dicionário em uma lista e pegando o vencedor pelo indice"""
+    result = available.items()
+    winner = sorted(result, key=itemgetter(1))
+    print(f'E o candidato escolhido foi',end='')
+    sleep(0.5)
+    print(f'.',end='')
+    sleep(0.5)
+    print(f'.',end='')
+    sleep(0.5)
+    print(f'.',end='')
+    sleep(1.5)
+    print(f'  {winner[2][0]}!')
+    sleep(0.5)
+    print('tivemos', available['Bolsonaro'], 'votos para o bolsonaro')
+    print('tivemos', available['Lula'], 'votos para o Lula')
+    print('tivemos', available['Batman'], 'votos para o Batman')
+    print('tivemos', unavailable['Nulo'], 'votos nulos')
+    print('tivemos', unavailable['Branco'], 'votos em branco')
 
 
